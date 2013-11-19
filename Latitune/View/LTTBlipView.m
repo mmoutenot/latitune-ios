7475187601ab102f4eeb492f7c7c5a3649695b7a
aldenks <aldenkeefesampson@gmail.com>
2013-11-18 20:23:21 -0500

    Radar View Controller tracks location, fetches nearby blips, and gives them to Radar View
    Radar View now calibrates location of blips, adds first few letter of song title for debugging

--- Latitune/View/LTTBlipView.m
+++ Latitune/View/LTTBlipView.m
@@ -19,6 +19,16 @@
     return self;
 }
 
+- (void)setBlip:(LTTBlip *)blip {
+  _blip = blip;
+  // Just a hack for now
+  UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(5, 5, 20, 20)];
+  titleLabel.text = [blip.song.title substringToIndex:3];
+  titleLabel.minimumScaleFactor = 0.1;
+  titleLabel.adjustsFontSizeToFitWidth = YES;
+  [self addSubview:titleLabel];
+}
+
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
